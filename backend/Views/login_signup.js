facb5256eeea7fdab799a2c59670479b8bc86561
Kanad Pardeshi <kanad5501@gmail.com>
2022-04-14 23:05:32 +0530

    Basic login backend, first attempt

--- backend/Views/login_signup.js
+++ backend/Views/login_signup.js
@@ -1,4 +1,7 @@
 const Pool = require('pg').Pool;
+const config = require('../config/auth.config')
+var jwt = require('jsonwebtoken')
+var bcrypt = require('bcrypt')
 require('dotenv').config();
 
 var user = process.env.USR;
@@ -15,16 +18,116 @@ const pool = new Pool({
     port: port
 });
 
-const check_login = asyn (req, res) => {
+const auto_user_id = async () => {
     try {
+	let len = await pool.query(
+	    `
+        select count(*) from AppUser;
+        `
+	);
+	len = len.rows[0].count;
+	let quer = pool.query(
+	    `
+	    create sequence if not exists inc
+        start with {$len}
+        increment by 1;
+        alter table AppUser
+        alter column User_ID
+        set default nextval('inc')
+        `);
+    } catch (err) {
+	return err.stack;
+    }
+}
+
+const checkIfExists = async (req, res, next) => {
+    try {
+	email = req.body.email;
+	let res = await pool.query(
+	    `
+        select *
+        from AppUser
+        where Emaill = $1
+        `, [email]
+	);
+	if (res.rows.length == 0) return true;
+	return false;
+    } catch (err) {
+		return err.stack;
+	}
+}
+
+
+const signup = async (req, res) => {
+    try {
+        let email = req.body.email;
+		let firstname = req.body.firstname;
+		let lastname = req.body.lastname;
+		let rollno = req.body.rollno;
+		let branch = req.body.branch;
+		let degree = req.body.degree;
+		let batch = req.body.batch;
+		let password = bcrypt.hashSync(req.body.password, 8);
+		let residence = req.body.residence;
+		let bday = req.body.bday;
+		let sgndate = new Date();
+		let pfp = req.body.pfp;
+		let pvt = req.body.pvt;
+		let autoadd = req.body.autoadd;
+
+		let ans = await pool.query(
+		    `
+ 	        insert into AppUser (First_Name, Last_Name, Roll_Number, Branch, Degree, Batch
+ 	                             Email, Hash_of_Password, Residence, Birthday, SignUp_Date,
+ 	                             Profile_Picture, Private, AutoAdd_to_Groups)
+ 	        values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+ 	        `,
+		 [firstname, lastname, rollno, branch, degree, batch, email, password,
+		  residence, bday, sgndate, pfp, pvt, autoadd]
+		)
+    } catch (err) {
+	return err.stack;
+    }
+}
+
+const login = async (req, res) => {
+    try {
+	email = req.body.email;
+	password = bcrypt.hashSync(req.body.password, 8);
         const res = await pool.query(
-            `
-            
-            `
+	    `
+        select *
+        from AppUser
+        where email = $1
+        `, [email]
         );
+	if (res.rows.length == 0) res.status(401).send({
+	    accessToken: null,
+	    message: "User doesn't exist"
+	});
+	ans = res.rows[0];
+	refpswd = ans['Hash_of_Password'];
+	var isValid = bcrypt.compareSync(password, refpswd);
+	if (!isValid) res.status(401).send({
+	    accessToken: null,
+	    message: 'Invalid Password'
+	});
+	var token = jwt.sign({id: ['User_ID']}, config.secret, {
+	    expiresIn: 86400
+	});
+	res.status(200).send({
+	    id: ans['User_ID'],
+	    email: ans['Email'],
+	    accessToken: token
+	});
         
     } catch (err) {
         return err.stack;
     }
 }
 
+module.exports = {
+    checkIfExists,
+    signup,
+    login
+}
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express();
+const login_signup = require ('./Views/login_signup')
 var corsOptions = {
     origin: 'http://localhost:8081'
 };
@@ -10,11 +11,15 @@ var corsOptions = {
 app.use(cors(corsOptions))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}));
+
 app.get('/', (req, res) => {
-    res.json({ message: 'Welcome'});
+    res.json({ message: 'Welcome to our project!'});
 });
 
 const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+app.post('/signup', [login_signup.checkIfExists], login_signup.signup);
+app.post('./login', login_signup.login);
