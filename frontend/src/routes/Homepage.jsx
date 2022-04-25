75740fe424b24379e7f0bcb51482913b383c1132
Manan Agarwal <mananagarwal007@gmail.com>
2022-04-25 18:44:31 +0530

    search box component

--- frontend/src/routes/Homepage.jsx
+++ frontend/src/routes/Homepage.jsx
@@ -1,10 +1,13 @@
 import React, { useContext, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import SearchBox from "../components/SeachBox";
 import { Context } from '../context/Context';
 
 const HomePage = () => {
     return (
-<div>Homepage!</div>
+<div>
+    <SearchBox/>
+    Homepage!</div>
     );
 };
 
