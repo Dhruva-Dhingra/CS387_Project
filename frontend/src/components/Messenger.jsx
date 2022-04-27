b9dedc1a94dd2f1d10d5ceca8712a6a807761559
Manan Agarwal <mananagarwal007@gmail.com>
2022-04-27 16:36:18 +0530

    Added messenger table

--- frontend/src/components/Messenger.jsx
+++ frontend/src/components/Messenger.jsx
@@ -1,13 +1,14 @@
 import React, { useState, useContext } from 'react';
-import HomepageFinder from '../apis/HomepageFinder';
+import MessageFinder from '../apis/MessageFinder';
 import { Context } from '../context/Context';
 
 const Messenger = (props) => {
     const {friends, setFriends} = useContext(Context);
+    let history = useNavigate();
     useEffect( ()=> {
         const fetchData = async () => {
             try {
-                const response = await  HomepageFinder.get("/messenger"); // TODO : add id as request parameter
+                const response = await  MessageFinder.get("/messenger"); // TODO : add id as request parameter
                 // console.log(response.data);
                 setFriends(response.data.data.friends);
             } catch (err) {}
@@ -15,8 +16,30 @@ const Messenger = (props) => {
 
         fetchData();
    },[]) 
-    return (
-        <div> Messenger</div>)
-}
+
+   const handleMsgSelect = (id) => {
+    history.push(`/homepage/${id}`);
+  };
+
+  return <div className='list-group'>
+  <table className="table table-hover table-dark table-striped table-bordered">
+      <thead>
+        <tr className='bg-primary'>
+            <th scope = "col">Friend's User ID</th>
+        </tr>
+      </thead>
+      <tbody>
+          {friends && friends.map(friend => {
+              return (
+                <tr onClick={() => handlePostSelect(friend.user_id)} 
+                key={friend.friend_id}>
+                  <td>{friend.friend_id}</td>
+              </tr>
+              )
+          })}
+      </tbody>
+  </table>
+</div>;
+};
 
 export default Messenger;
