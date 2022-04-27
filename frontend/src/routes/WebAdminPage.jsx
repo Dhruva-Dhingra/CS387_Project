ba7b2ff6f8999fb6acea0f43a6a2fa01b3a2ad99
Manan Agarwal <mananagarwal007@gmail.com>
2022-04-27 21:04:26 +0530

    added friends vs likes plot

--- frontend/src/routes/WebAdminPage.jsx
+++ frontend/src/routes/WebAdminPage.jsx
@@ -7,7 +7,7 @@ import { useNavigate } from "react-router-dom";
 
 const WebAdminPage = () => {
     
-    const {L1, SL1, D1, SD1} = useContext(Context);
+    const {L1, SL1, D1, SD1, L2, SL2, D2, SD2} = useContext(Context);
     let history = useNavigate();
     useEffect( ()=> {
          const fetchData = async () => {
@@ -15,6 +15,8 @@ const WebAdminPage = () => {
                  const response = await  AdminFinder.get("/");
                  SL1(response.data.data.num_friends);
                  SD1(response.data.data.frequency);
+                 SL2(response.data.data.num_friends);
+                 SD2(response.data.data.num_likes);
              } catch (err) {}
          }
          fetchData();
@@ -52,6 +54,33 @@ const WebAdminPage = () => {
         display:true,
         position:'top'
       },}}}/>
+
+{/* NUMBER OF FRIENDS VS AVG NUMBER OF LIKES */}
+<Line
+      data = {{
+                 labels: L2.map((B) => B.num_friends),
+                 type: 'line',
+                 datasets : [
+                 {
+                  type: 'line',
+                  fill: false,
+                  lineTension: 0.5,
+                  borderColor:'rgb(54, 162, 235)',
+                  backgroundColor: 'rgb(54, 162, 235)',
+                  borderWidth: 2,
+                  pointRadius: 0,
+                data: D2.map ((A) => A.num_likes)
+                    }],}}
+      options = {{plugins:{title:{
+        display:true,
+        text:'Number of Friends Vs Number of Likes',
+        fontSize:50
+      },
+      legend:{
+        display:true,
+        position:'top'
+      },}}}/>
+
 
 </div>
     
