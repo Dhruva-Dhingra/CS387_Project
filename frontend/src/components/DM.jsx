a1e640f88dae7c51ed977cc75b0a6e7b2dd613e0
Tanu Goyal <tanugoyal.iitb@gmail.com>
2022-04-28 21:47:07 +0530

    minor changes

--- frontend/src/components/DM.jsx
+++ frontend/src/components/DM.jsx
@@ -5,11 +5,11 @@ import { useNavigate } from "react-router-dom";
 const DM = (props) => {
     const {msgs, setMsg} = useContext(Context);
     let history = useNavigate();
-    useEffect( ()=> {
+    useEffect( (id)=> {
         const fetchData = async () => {
             try {
-                const response = await  MessageFinder.get("/message/:id"); // TODO : add id as request parameter
-                // console.log(response.data);
+                const response = await  MessageFinder.get("/:id"); // TODO : add id as request parameter
+                console.log(response.data);
                 setMsg(response.data.data.result);
             } catch (err) {}
         }
@@ -17,9 +17,9 @@ const DM = (props) => {
         fetchData();
    },[]) 
 
-   const handleMsgSelect = (id) => {
-    history.push(`/message/${id}`);
-  };
+//    const handleMsgSelect = (id) => {
+//     history.push(`/messenger/${id}`);
+//   };
 
   return <div className='list-group'>
   <table className="table table-hover table-dark table-striped table-bordered">
@@ -32,7 +32,7 @@ const DM = (props) => {
       <tbody>
           {msgs && msgs.map(msg => {
               return (
-                <tr onClick={() => handleMsgSelect(msg.user_id)} 
+                <tr
                 key={msg.msg_id}>
                   <td>{msg.msg_content}</td>
                   <td>{msg.time}</td>
