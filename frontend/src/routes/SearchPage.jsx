7cd3f932dacbb52b4f338cb188ca94456d68e3f6
Manan Agarwal <62429009+MananAg007@users.noreply.github.com>
2022-04-28 19:34:02 +0530

    SearchBox frontend

--- frontend/src/routes/SearchPage.jsx
+++ frontend/src/routes/SearchPage.jsx
@@ -1,15 +1,48 @@
 import React, { useContext, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import SearchBoxFinder from "../apis/SearchBoxFinder";
 import DisplayPostHomepage from "../components/DisplayPostHomepage";
 import SearchBox from "../components/SearchBox"
 import { Context } from '../context/Context';
 
 const SearchPage = () => {
-    return (
-<div>
-    <SearchBox/>
-    Your Searches.</div>
-    );
+    
+    const {L1, SL1} = useContext(Context);
+
+    useEffect( ()=>{
+        const fetchData = async () => {
+            try {
+                const response = await SearchBoxFinder.get("/");
+                console.log(response);
+                SL1 (response.data.data.results);
+            } catch (err) {console.log(err.stack);}   
+        }
+        fetchData();
+    },[])
+    
+    return <div className='list-group'>
+        <SearchBox/>
+        Your Searches.
+    <table className="table table-hover table-dark table-striped table-bordered">
+        <thead>
+          <tr className='bg-primary'>
+              {/* <th scope = "col">Post ID</th> */}
+              <th scope = "col">User ID list</th>
+          </tr>
+        </thead>
+        <tbody>
+            {L1 && L1.map(search => {
+                return (
+                  <tr onClick={() => handlePostSelect(search.user_id)} 
+                  key={search.user_id}>
+                    {/* <td>{posts.post_id}</td> */}
+                    <td>{search.user_id}</td>
+                </tr>
+                )
+            })}
+        </tbody>
+    </table>
+</div>;
 };
 
 export default SearchPage;
