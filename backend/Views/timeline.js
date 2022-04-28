c0287d248cc087018c4e33061d525902b6a35534
Dhruva Dhingra <dhruva.dhingra@gmail.com>
2022-04-29 02:11:38 +0530

    Updated timeline query

--- backend/Views/timeline.js
+++ backend/Views/timeline.js
@@ -28,12 +28,27 @@ const get_timeline = async (req, res) => {
         let start = req.body.start;
         let end = req.body.end;
         pool.query(
-            `select post_info.post_id, post_info.poster_page_id, post_info.poster_user_id, post_info.content_type, post_info.content, post_info.time, coalesce(reaction_count.reaction_count, 0)
-            from 
-            (select Post.Post_id, Post.Page_ID as poster_page_id, Post.User_ID as poster_user_id, Post.Content_Type, Post.Content, Post.Time, row_number() over (Partition By Post.User_ID order by Post.Time desc) as Post_Rank
-                from Post where Post.User_ID = $1
-            ) as post_info left outer join reaction_count
-            on post_info.post_rank between $2 and $3 and (post_info.post_id = reaction_count.post_id or reaction_count.post_id is null);`,
+            // `select post_info.post_id, post_info.poster_page_id, post_info.poster_user_id, post_info.content_type, post_info.content, post_info.time, coalesce(reaction_count.reaction_count, 0)
+            // from 
+            // (select Post.Post_id, Post.Page_ID as poster_page_id, Post.User_ID as poster_user_id, Post.Content_Type, Post.Content, Post.Time, row_number() over (Partition By Post.User_ID order by Post.Time desc) as Post_Rank
+            //     from Post where Post.User_ID = $1
+            // ) as post_info left outer join reaction_count
+            // on post_info.post_rank between $2 and $3 and (post_info.post_id = reaction_count.post_id or reaction_count.post_id is null);`,
+            `
+            with post_info_2 as (select post_info.post_id as post_id, post_info.poster_page_id as page_id, post_info.poster_user_id as user_id, post_info.content_type as content_type, post_info.content as content, post_info.time as time, post_rank, coalesce(count(reaction.reaction), 0) as reaction_count
+            from (
+                select post_id, page_id as poster_page_id, user_id as poster_user_id, content_type, content, time, row_number() over (order by time DESC) as post_rank
+                from post
+                where
+                Post.User_ID = $1
+            ) as post_info left outer join reaction
+            on post_info.post_rank between $2 and $3 and (post_info.post_id = reaction.post_id or reaction.post_id is null)
+            group by (post_info.post_id, post_info.poster_page_id, post_info.poster_user_id, post_info.content_type, post_info.content, post_info.time, post_rank)),
+		    user_info as (select user_id, first_name, last_name, email from appuser where user_id = $1) 
+            select post_info_2.post_id as post_id, post_info_2.page_id as page_id, post_info_2.user_id as user_id, first_name, last_name, email, post_info_2.content_type as content_type, post_info_2.content as content, post_info_2.time as time, post_info_2.reaction_count
+            from (post_info_2 join user_info
+            on post_info_2.user_id = user_info.user_id) WHERE post_rank BETWEEN $2 AND $3 order by time DESC
+		    ;`,
             [user_id, start, end],
 			(err, result) => {
 				if (err) {
