186497a44b95f36d58c714c1627b81caab246d1b
Dhruva Dhingra <dhruva.dhingra@gmail.com>
2022-04-29 17:23:09 +0530

    Updated timeline backend query

--- backend/Views/timeline.js
+++ backend/Views/timeline.js
@@ -50,8 +50,8 @@ const get_timeline = async (req, res) => {
             ) as post_info left outer join reaction
             on post_info.post_rank between $2 and $3 and (post_info.post_id = reaction.post_id or reaction.post_id is null)
             group by (post_info.post_id, post_info.poster_page_id, post_info.poster_user_id, post_info.content_type, post_info.content, post_info.time, post_rank)),
-		    user_info as (select user_id, first_name, last_name, email from appuser where user_id = $1) 
-            select post_info_2.post_id as post_id, post_info_2.page_id as page_id, post_info_2.user_id as user_id, first_name, last_name, email, post_info_2.content_type as content_type, post_info_2.content as content, post_info_2.time as time, post_info_2.reaction_count
+		    user_info as (select user_id, first_name, last_name, roll_number, branch, degree, batch, email, residence, birthday, signup_date from appuser where user_id = $1) 
+            select post_info_2.post_id as post_id, post_info_2.page_id as page_id, post_info_2.user_id as user_id, first_name, last_name, roll_number, branch, degree, batch, email, residence, birthday, signup_date, post_info_2.content_type as content_type, post_info_2.content as content, post_info_2.time as time, post_info_2.reaction_count
             from (post_info_2 join user_info
             on post_info_2.user_id = user_info.user_id) WHERE post_rank BETWEEN $2 AND $3 order by time DESC
 		    ;`,
