bb5f295fc2464a3f3481d27e039e441d70ec6341
Kanad Pardeshi <kanad5501@gmail.com>
2022-04-29 16:24:28 +0530

    Login interface fixed

--- frontend/src/routes/LoginSignup.jsx
+++ frontend/src/routes/LoginSignup.jsx
@@ -45,14 +45,14 @@ const LoginForm = () => {
 				<Form onSubmit={handleSubmit}>
 						<Form.Group className="mb-3" controlId="formBasicEmail">
 								<Form.Label>Email address</Form.Label>
-								<Form.Control type="email" name='email' placeholder="Enter email" value={state.email} onChange={handleChange}/>
+								<Form.Control type="email" name='email' placeholder="Enter email" onChange={handleChange}/>
 								<Form.Text className="text-muted">
 										We'll never share your email with anyone else.
 								</Form.Text>
 						</Form.Group>	
 						<Form.Group className="mb-3" controlId="formBasicPassword">
 								<Form.Label>Password</Form.Label>
-								<Form.Control type="password" name='password' placeholder="Password" value={state.password} onChange={handleChange} />
+								<Form.Control type="password" name='password' placeholder="Password" onChange={handleChange}/>
 						</Form.Group>
 						<Button variant="primary" type="submit">
 								Submit
