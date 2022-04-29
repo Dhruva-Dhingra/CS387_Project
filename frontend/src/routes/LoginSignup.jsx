92fb388099755e6aedf1e4db1e148107c373375c
Manan Agarwal <mananagarwal007@gmail.com>
2022-04-29 12:47:20 +0530

    signup update

--- frontend/src/routes/LoginSignup.jsx
+++ frontend/src/routes/LoginSignup.jsx
@@ -67,114 +67,112 @@ class LoginForm extends Component {
 }
 
 class SignupForm extends Component {
-		constructor() {
-				super();
-
-				this.state = {
-						// firstname: null,
-						// lastname: null,
-						// rollno: null,
-						// branch: null,
-						// degree: null,
-						// batch: null,
-						email: '',
-						password: '',
-						// residence: null,
-						// birthday: null,
-						// profile_pic: null,
-						// pvt: null,
-						// autoadd: null,
-				}
-				this.handleSubmit = this.handleSubmit.bind(this);
-				this.handleChange = this.handleChange.bind(this);
+	constructor() {
+		super();
+  
+		this.state = {
+			firstname: '',
+			lastname: '',
+			rollno: '',
+			branch: '',
+			degree: '',
+			batch: '',
+			email: '',
+			password: '',
+			residence: '',
+			birthday: '',
+			profile_pic: '',
+			
 		}
-
-		handleChange(e) {
-				e.preventDefault();
-				console.log('Change in signup form');
-				this.setState({
-						[e.target.name]: e.target.value,
-				});
-				console.log(this.state);
-		}
-		
-		handleSubmit(e) {
-				e.preventDefault();
-				console.log('Sending to backend!');
-				console.log(this.state);
-				const requestOptions = {
-						method: 'POST',
-						headers: { 'Content-Type': 'application/json'},
-						body: JSON.stringify(this.state),
-				};
-				fetch('http://localhost:8080/signup', requestOptions)
-						.then(response => {
-								response.json();
-								console.log(response);
-						});
-		}
-
-		render() {
-				return (
-						<div>
-								<p>Please sign up!</p>
-								<br/>
-								<form onSubmit = {this.handleSubmit}  class="was-validated">
-										
-										
-										<div className="col">
-												First Name:
-												<input type = 'text' className='form-control' name = 'email' value = {this.state.email} placeholder = 'Enter email' onChange = {this.handleChange} />
-										</div>
-		
-										<div className='col'>
-												Last Name:
-												<input type = 'password' className='form-control' name = 'password' placeholder = 'password' value = {this.state.password} onChange = {this.handleChange} />
-										</div>
-										<div className='col'>
-												Roll Number:
-												<input type = 'password' className='form-control' name = 'password' placeholder = 'password' value = {this.state.password} onChange = {this.handleChange} />
-										</div>
-										<div className='col'>
-												Branch:
-												<input type = 'password' className='form-control' name = 'password' placeholder = 'password' value = {this.state.password} onChange = {this.handleChange} />
-										</div>
-										<div className='col'>
-												Degree:
-												<input type = 'password' className='form-control' name = 'password' placeholder = 'password' value = {this.state.password} onChange = {this.handleChange} />
-										</div>
-										<div className='col'>
-												Batch:
-												<input type = 'password' className='form-control' name = 'password' placeholder = 'password' value = {this.state.password} onChange = {this.handleChange} />
-										</div>
-										<div className='col'>
-												Email-ID:
-												<input type = 'password' className='form-control' name = 'password' placeholder = 'password' value = {this.state.password} onChange = {this.handleChange} />
-										</div>
-										<div className='col'>
-												Password:
-												<input type = 'password' className='form-control' name = 'password' placeholder = 'password' value = {this.state.password} onChange = {this.handleChange} />
-										</div>
-										<div className='col'>
-												Residence:
-												<input type = 'password' className='form-control' name = 'password' placeholder = 'password' value = {this.state.password} onChange = {this.handleChange} />
-										</div>
-										<div className='col'>
-												Birthday:
-												<input type = 'password' className='form-control' name = 'password' placeholder = 'password' value = {this.state.password} onChange = {this.handleChange} />
-										</div>
-										<div className='col'>
-												Profile Picture:
-												<input type = 'password' className='form-control' name = 'password' placeholder = 'password' value = {this.state.password} onChange = {this.handleChange} />
-										</div>
-										<br/>
-										<center><input type = 'submit' value ='Submit'></input></center>
-								</form>
-						</div>
-				);
-		}
-
-}
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+	}
+  
+	handleChange(e) {
+		e.preventDefault();
+		console.log('Change in signup form');
+		this.setState({
+			[e.target.name]: e.target.value,
+		});
+		console.log(this.state);
+	}
+	
+	handleSubmit(e) {
+		e.preventDefault();
+		console.log('Sending to backend!');
+		console.log(this.state);
+		const requestOptions = {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json'},
+			body: JSON.stringify(this.state),
+		};
+		fetch('http://localhost:8080/signup', requestOptions)
+			.then(response => {
+				response.json();
+				console.log(response);
+			});
+	}
+  
+	render() {
+		return (
+			<div>
+				<center>Sign up!</center>
+				<form onSubmit = {this.handleSubmit}  class="was-validated">
+					
+					
+					<div className="col">
+						First Name:
+						<input type = 'text' className='form-control' name = 'email' value = {this.state.firstname} placeholder = 'Your First Name' onChange = {this.handleChange} />
+					</div>
+	
+					<div className='col'>
+						Last Name:
+						<input type = 'text' className='form-control' name = 'password' placeholder = 'Your Last Name' value = {this.state.lastname} onChange = {this.handleChange} />
+					</div>
+					<div className='col'>
+						Roll Number:
+						<input type = 'text' className='form-control' name = 'password' placeholder = 'Roll Number' value = {this.state.rollno} onChange = {this.handleChange} />
+					</div>
+					<div className='col'>
+						Branch:
+						<input type = 'text' className='form-control' name = 'password' placeholder = 'Branch' value = {this.state.branch} onChange = {this.handleChange} />
+					</div>
+					<div className='col'>
+						Degree:
+						<input type = 'text' className='form-control' name = 'password' placeholder = 'Degree' value = {this.state.degree} onChange = {this.handleChange} />
+					</div>
+					<div className='col'>
+						Batch:
+						<input type = 'number' className='form-control' name = 'password' placeholder = 'Batch' value = {this.state.batch} onChange = {this.handleChange} />
+					</div>
+					<div className='col'>
+						Email-ID:
+						<input type = 'text' className='form-control' name = 'password' placeholder = 'Enter your Email-ID' value = {this.state.email} onChange = {this.handleChange} />
+					</div>
+					<div className='col'>
+						Password:
+						<input type = 'password' className='form-control' name = 'password' placeholder = 'Password' value = {this.state.password} onChange = {this.handleChange} />
+					</div>
+					<div className='col'>
+						Residence:
+						<input type = 'text' className='form-control' name = 'password' placeholder = 'Residence' value = {this.state.residence} onChange = {this.handleChange} />
+					</div>
+					<div className='col'>
+						Birthday:
+						<input type = 'date' className='form-control' name = 'password' placeholder = 'Birthday' value = {this.state.birthday} onChange = {this.handleChange} />
+					</div>
+					<div className='col'>
+						Profile Picture:
+						<input type = 'file' className='form-control' name = 'password' placeholder = 'Select your Profile Picture' value = {this.state.profile_pic} onChange = {this.handleChange} />
+					</div>
+					<br/>
+					<center><input type = 'submit' className="btn btn-warning btn-lg" value ='Submit'></input></center>
+				</form>
+			</div>
+		);
+	}
+  
+  }
 
 class LoginSignup extends Component {
 		constructor () {
